ff2391c3bddc1c47cf58721b731f45ed9a1e908f
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2023-03-28 17:01:53 +0300

    corrected JS Reduce button for cards

--- js/js-dropdown.js
+++ js/js-dropdown.js
@@ -2,45 +2,28 @@
 const dropdownButtons = document.querySelectorAll('button.dropdown__button');
 
 // Dropdown list
-const dropdownLists = document.querySelectorAll('ul.dropdown__menu');
+const dropdownContents = document.querySelectorAll('ul.dropdown__menu');
 
-// Loop through each dropdown button and add event listener
-dropdownButtons.forEach(function(button){
-    button.addEventListener("click", function(){
-        // Get the corresponding dropdown list for this button
-        const dropdownList = this.nextElementSibling;
+// .burgers__component
+const burgersComponents = document.querySelectorAll('li.burgers__component');
 
-        // Toggle the dropdown visibility
-        dropdownList.classList.toggle('dropdown__menu--visible');
+// Event Listener
+dropdownButtons.forEach((button, index) => {
+  button.addEventListener("click", () => onClick(index));
+});
 
-        // Toggle the button text
-    if (dropdownList.classList.contains('dropdown__menu--visible')) {
-      this.dropdownButton.style.top = 350;
-      this.dropdownButton.textContent = "Reduce";
-      this.burgersComponent.style.paddingBottom = 60;
-    } else {
-      this.textContent = 'Details';
-      this.dropdownButton.style.top = 210;
-      this.burgersComponent.style.paddingBottom = 20;
-    }
-    })
-})
+// Function
+function onClick(index){
 
-
-// //Function
-// function onClick(){
-
-//      if(dropdownContent.classList.contains("dropdown__menu--visible")){
-//         dropdownContent.classList.remove("dropdown__menu--visible");
-//         dropdownButton.textContent = "Details";
-//         dropdownButton.style.top = 210;
-//         burgersComponent.style.paddingBottom = 20;
-
-
-//     } else {
-//         dropdownContent.classList.add("dropdown__menu--visible");
-//         dropdownButton.style.top = 350;
-//         dropdownButton.textContent = "Reduce";
-//         burgersComponent.style.paddingBottom = 60;
-//     }
-// }       
+  if(dropdownContents[index].classList.contains("dropdown__menu--visible")){
+    dropdownContents[index].classList.remove("dropdown__menu--visible");
+    dropdownButtons[index].textContent = "Details";
+    dropdownButtons[index].style.top = 210;
+    burgersComponents[index].style.paddingBottom = 20;
+  } else {
+    dropdownContents[index].classList.add("dropdown__menu--visible");
+    dropdownButtons[index].style.top = 350;
+    dropdownButtons[index].textContent = "Reduce";
+    burgersComponents[index].style.paddingBottom = 60;
+  }
+}
