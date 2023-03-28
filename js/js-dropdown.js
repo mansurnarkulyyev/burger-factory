79d83bf683041042abdc3e7ed5a430c898f1d109
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2023-03-28 16:54:30 +0300

    added dropdown JS to all card components

--- js/js-dropdown.js
+++ js/js-dropdown.js
@@ -1,29 +1,46 @@
 // Dropdown button
-const dropdownButton = document.querySelector('button.dropdown__button');
+const dropdownButtons = document.querySelectorAll('button.dropdown__button');
 
 // Dropdown list
-const dropdownContent = document.querySelector('ul.dropdown__menu');
+const dropdownLists = document.querySelectorAll('ul.dropdown__menu');
 
-// .burgers__component
-burgersComponent = document.querySelector('li.burgers__component');
+// Loop through each dropdown button and add event listener
+dropdownButtons.forEach(function(button){
+    button.addEventListener("click", function(){
+        // Get the corresponding dropdown list for this button
+        const dropdownList = this.nextElementSibling;
 
-// Event Listener
-dropdownButton.addEventListener("click", onClick);
+        // Toggle the dropdown visibility
+        dropdownList.classList.toggle('dropdown__menu--visible');
 
-//Function
-function onClick(){
-
-     if(dropdownContent.classList.contains("dropdown__menu--visible")){
-        dropdownContent.classList.remove("dropdown__menu--visible");
-        dropdownButton.textContent = "Details";
-        dropdownButton.style.top = 210;
-        burgersComponent.style.paddingBottom = 20;
-
-
+        // Toggle the button text
+    if (dropdownList.classList.contains('dropdown__menu--visible')) {
+      this.dropdownButton.style.top = 350;
+      this.dropdownButton.textContent = "Reduce";
+      this.burgersComponent.style.paddingBottom = 60;
     } else {
-        dropdownContent.classList.add("dropdown__menu--visible");
-        dropdownButton.style.top = 350;
-        dropdownButton.textContent = "Reduce";
-        burgersComponent.style.paddingBottom = 60;
+      this.textContent = 'Details';
+      this.dropdownButton.style.top = 210;
+      this.burgersComponent.style.paddingBottom = 20;
     }
-}       
+    })
+})
+
+
+// //Function
+// function onClick(){
+
+//      if(dropdownContent.classList.contains("dropdown__menu--visible")){
+//         dropdownContent.classList.remove("dropdown__menu--visible");
+//         dropdownButton.textContent = "Details";
+//         dropdownButton.style.top = 210;
+//         burgersComponent.style.paddingBottom = 20;
+
+
+//     } else {
+//         dropdownContent.classList.add("dropdown__menu--visible");
+//         dropdownButton.style.top = 350;
+//         dropdownButton.textContent = "Reduce";
+//         burgersComponent.style.paddingBottom = 60;
+//     }
+// }       
