530fdc9eaa9b8f92f30e5ed3e5f873320ca9a60f
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2023-03-28 17:20:28 +0300

    Ingredients slider responsive corrected images count

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,21 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
+  // slidesPerView: 4,
+  // spaceBetween: 30,
   pagination: {
-    // el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    }
+  }
 });
